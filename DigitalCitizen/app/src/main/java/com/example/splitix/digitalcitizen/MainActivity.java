45d56a967bb10bd5a4502fd4c66da19c702a4adc
Joshua Galindo <Splitusa@gmail.com>
2015-11-14 10:56:20 -0600

    stuff

--- DigitalCitizen/app/src/main/java/com/example/splitix/digitalcitizen/MainActivity.java
+++ DigitalCitizen/app/src/main/java/com/example/splitix/digitalcitizen/MainActivity.java
@@ -58,7 +58,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Super Hidden message because peter is amazing!", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
