a086e5070448616d7fb7d1f26c19fa030b85ac3e
Josh Granberry <jdgranberry@gmail.com>
2015-11-14 17:01:19 -0600

    Added event container class for event details

---
+++ DCitizen/app/src/main/java/com/example/splitix/dcitizen/Events/Event.java
@@ -0,0 +1,11 @@
+package com.example.splitix.dcitizen;
+
+/**
+ * Created by jdgranberry on 11/14/15.
+ */
+public class Event extends Object {
+    private String title;
+    private String description;
+
+
+}
