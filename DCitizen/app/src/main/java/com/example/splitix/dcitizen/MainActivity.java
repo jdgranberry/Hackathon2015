c9933b25d5bf486e7f07392c2cbb9c8237964f95
Josh Granberry <jdgranberry@gmail.com>
2015-11-14 17:05:55 -0600

    Linked fab to event submission page

--- DCitizen/app/src/main/java/com/example/splitix/dcitizen/MainActivity.java
+++ DCitizen/app/src/main/java/com/example/splitix/dcitizen/MainActivity.java
@@ -42,6 +42,7 @@ public class MainActivity extends AppCompatActivity
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                startActivity(new Intent(MainActivity.this, EventSubmissionActivity.class));
             }
         });
 
