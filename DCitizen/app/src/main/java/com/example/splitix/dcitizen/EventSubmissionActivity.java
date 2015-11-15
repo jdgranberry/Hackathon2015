95a87ad4718eb3f7877acea0fefa570fda8feb94
Josh Granberry <jdgranberry@gmail.com>
2015-11-15 00:36:10 -0600

    title font

--- DCitizen/app/src/main/java/com/example/splitix/dcitizen/EventSubmissionActivity.java
+++ DCitizen/app/src/main/java/com/example/splitix/dcitizen/EventSubmissionActivity.java
@@ -2,19 +2,25 @@ package com.example.splitix.dcitizen;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 import com.example.splitix.dcitizen.Events.EventContainer;
 
 public class EventSubmissionActivity extends AppCompatActivity {
 
+    ImageButton submit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_submission);
 
         EventContainer newEvent;
-        String newTitle;
-        String newDesc;
-        String tags;
+
+        EditText mTitle;
+        EditText mBlurb;
+        EditText mTags;
+
+
     }
 }
