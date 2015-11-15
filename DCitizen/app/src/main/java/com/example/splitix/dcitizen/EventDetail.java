c4c811d3fb554392750bfe31083029b698077a1b
spectechular <mramirezt4@hotmail.com>
2015-11-15 12:57:32 -0600

    Detail Activity with image displayed

--- DCitizen/app/src/main/java/com/example/splitix/dcitizen/EventDetail.java
+++ DCitizen/app/src/main/java/com/example/splitix/dcitizen/EventDetail.java
@@ -1,9 +1,15 @@
 package com.example.splitix.dcitizen;
 
+import android.content.Intent;
+import android.media.Image;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.android.gms.maps.GoogleMap;
@@ -11,10 +17,17 @@ import com.google.android.gms.maps.MapFragment;
 
 import org.w3c.dom.Text;
 
+import java.util.Locale;
+
 public class EventDetail extends AppCompatActivity {
+
+    final String longitude = "30.269650";
+    final String latitude ="-97.760140";
 
     private TextView title_view;
     private TextView description_view;
+    private ImageView imageView;
+    private ImageButton mapButton;
     private String eventType;
     private Bundle extras;
     private String title;
@@ -28,6 +41,8 @@ public class EventDetail extends AppCompatActivity {
 
         title_view = (TextView) findViewById(R.id.event_detail_title);
         description_view = (TextView) findViewById(R.id.event_detail_description);
+        imageView =(ImageView) findViewById(R.id.event_image);
+        mapButton = (ImageButton) findViewById(R.id.map_image);
 
         extras = getIntent().getExtras();
 
@@ -38,6 +53,16 @@ public class EventDetail extends AppCompatActivity {
 
         title_view.setText(title);
         description_view.setText(description);
+
+        final String uri = String.format("geo:0,0?q=" + latitude+ "," + longitude);
+
+        mapButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
+                startActivity(mapIntent);
+            }
+        });
 
 
     }
